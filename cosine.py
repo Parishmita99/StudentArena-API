3b76176e11048639056b1f96a0f00181bf8a9205
Parishmita Das <55506586+Parishmita99@users.noreply.github.com>
2021-06-19 00:24:34 +0530

    Update cosine.py

--- cosine.py
+++ cosine.py
@@ -3,33 +3,35 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-df=pd.read_csv("dataset/Preprocess.csv.zip")
+df=pd.read_csv("Preprocessed.csv",nrows=10000)
 cv = CountVectorizer()
+df.fillna(" ")
+
 count_matrix = cv.fit_transform(df['combined'])
 cosine_sim = cosine_similarity(count_matrix)
 
 def get_index_from_job_type(title):
     return df[df['jobtitle']==title]['Job id'].values[0]
 
-def recommend(job):
-  jobs_user_likes =job
+def recommend(jobs_user_likes):
   Job_id = get_index_from_job_type(jobs_user_likes)
   similar_jobs = list(enumerate(cosine_sim[Job_id]))
   sorted_similar_jobs = sorted(similar_jobs,key=lambda x:x[1],reverse=True)[1:]
-  print("Top 10 similar jobs to "+job+" are:\n")
-  return sorted_similar_jobs
-k=recommend("java architect - denver, co - fulltime")
-i=0
+  sorted_similar_jobs = sorted_similar_jobs[1:21]
+    
+  job_indices = [i[0] for i in sorted_similar_jobs]
+  job_id = df['Job id'].iloc[job_indices]
+  job_title = df['jobtitle'].iloc[job_indices]
+  job_desc = df['jobdescription'].iloc[job_indices]
+  job_company = df['company'].iloc[job_indices]
+  recommendation_data = pd.DataFrame(columns=['Job_Id','Job title','Job Description','Company'])
 
-y_pred=[]
-for element in k:
-    if i==0:
-      y_pred=df.iloc[(element[0])]
-    #print(df.iloc[(element[0])])
-    i=i+1
-    if i>0:
-      y_pred=y_pred.append(df.iloc[(element[0])])
-    if i>10:
-        break
-for i in y_pred:
-  print(i)
+  recommendation_data['Job_Id'] = job_id
+  recommendation_data['Job title'] = job_title
+  recommendation_data['Job Description'] = job_desc
+  recommendation_data['Company'] = job_company
+  return recommendation_data
+
+jobs_user_likes ="java architect - denver, co - fulltime"
+recommendation_data= recommend(jobs_user_likes)
+print(recommendation_data)
