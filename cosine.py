1e8c6d73462831d49b97e121d3adf8cfb40550ce
Parishmita Das <55506586+Parishmita99@users.noreply.github.com>
2021-08-02 13:27:31 +0530

    Update cosine.py

--- cosine.py
+++ cosine.py
@@ -11,7 +11,7 @@ count_matrix = cv.fit_transform(df['combined'])
 cosine_sim = cosine_similarity(count_matrix)
 
 def get_index_from_job_type(title):
-    return df[df['jobtitle']==title]['Job id'].values[0]
+    return df[df['jobtitle']==title]['Job_id'].values[0]
 
 def recommend(jobs_user_likes):
     if jobs_user_likes not in df['jobtitle'].unique():
@@ -23,15 +23,15 @@ def recommend(jobs_user_likes):
         sorted_similar_jobs = sorted_similar_jobs[1:21]
     
         job_indices = [i[0] for i in sorted_similar_jobs]
-        job_id = df['Job id'].iloc[job_indices]
+        job_id = df['Job_id'].iloc[job_indices]
         job_title = df['jobtitle'].iloc[job_indices]
         job_desc = df['jobdescription'].iloc[job_indices]
         job_company = df['company'].iloc[job_indices]
-        recommendation_data = pd.DataFrame(columns=['Job_Id','Job title','Job Description','Company'])
+        recommendation_data = pd.DataFrame(columns=['Job_Id','Job_title','Job_Description','Company'])
 
         recommendation_data['Job_Id'] = job_id
-        recommendation_data['Job title'] = job_title
-        recommendation_data['Job Description'] = job_desc
+        recommendation_data['Job_title'] = job_title
+        recommendation_data['Job_Description'] = job_desc
         recommendation_data['Company'] = job_company
         return recommendation_data.to_dict('records')
 
