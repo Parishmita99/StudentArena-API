44064ef90104c408d59b32011e026d8b98144e0c
Parishmita Das <55506586+Parishmita99@users.noreply.github.com>
2021-08-02 17:51:56 +0530

    Update cosine.py

--- cosine.py
+++ cosine.py
@@ -27,11 +27,11 @@ def recommend(jobs_user_likes):
         job_title = df['jobtitle'].iloc[job_indices]
         job_desc = df['jobdescription'].iloc[job_indices]
         job_company = df['company'].iloc[job_indices]
-        recommendation_data = pd.DataFrame(columns=['Job_id','Job title','Job Description','Company'])
+        recommendation_data = pd.DataFrame(columns=['Job_id','Job_title','Job_Description','Company'])
 
         recommendation_data['Job_id'] = job_id
-        recommendation_data['Job title'] = job_title
-        recommendation_data['Job Description'] = job_desc
+        recommendation_data['Job_title'] = job_title
+        recommendation_data['Job_Description'] = job_desc
         recommendation_data['Company'] = job_company
         return recommendation_data.to_dict('records')
 
